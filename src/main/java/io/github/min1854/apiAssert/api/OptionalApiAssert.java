3a5c554b8759ec33f10d3ff4f2dd67dd04a560be
min <min@1854.com>
2026-07-04 22:47:18 +0800

    OptionalApiAssert：为 isNull、nonNull、isEmpty、isTrue、isFalse 方法新增 Supplier<Message> 参数，为了能够实现 message 更方便的计算、获取，并更改为默认方法。process 方法改为默认方法，由接口实现，新增 get 方法，返回 ELEMENT 用以 process 方法调用。 AbstractOperationApiAssert：将重复实现的方法去除。

--- src/main/java/io/github/min1854/apiAssert/api/OptionalApiAssert.java
+++ src/main/java/io/github/min1854/apiAssert/api/OptionalApiAssert.java
@@ -5,41 +5,98 @@ import java.util.function.*;
 public interface OptionalApiAssert<ELEMENT extends ACTUAL, SELF extends OptionalApiAssert<ELEMENT, SELF, MESSAGE, ACTUAL>, MESSAGE, ACTUAL>
         extends StandardApiAssert<ACTUAL, SELF, MESSAGE> {
 
-    public SELF nonNull(MESSAGE message);
+    default SELF isNull(MESSAGE message) {
+        return isNull(Function.identity(), message);
+    }
 
-    public <R extends ACTUAL> SELF nonNull(Function<ELEMENT, R> function, MESSAGE message);
+    default SELF isNull(Supplier<MESSAGE> message) {
+        return isNull(Function.identity(), message);
+    }
 
-    public <R extends ACTUAL> SELF nonNull(Function<ELEMENT, R> function, Function<ELEMENT, MESSAGE> message);
+    default <R extends ACTUAL> SELF isNull(Function<ELEMENT, R> function, MESSAGE message) {
+        return isNull(function, v -> message);
+    }
 
-    public SELF isNull(MESSAGE message);
+    default <R extends ACTUAL> SELF isNull(Function<ELEMENT, R> function, Supplier<MESSAGE> message) {
+        return isNull(function, v -> message.get());
+    }
 
-    public <R extends ACTUAL> SELF isNull(Function<ELEMENT, R> function, MESSAGE message);
+    <R extends ACTUAL> SELF isNull(Function<ELEMENT, R> function, Function<ELEMENT, MESSAGE> message);
 
-    public <R extends ACTUAL> SELF isNull(Function<ELEMENT, R> function, Function<ELEMENT, MESSAGE> message);
+    default SELF nonNull(MESSAGE message) {
+        return nonNull(Function.identity(), message);
+    }
 
-    public <R extends ACTUAL> SELF isEmpty(Function<ELEMENT, R> function, MESSAGE message);
+    default SELF nonNull(Supplier<MESSAGE> message) {
+        return nonNull(Function.identity(), message);
+    }
 
-    public SELF isEmpty(MESSAGE message);
+    default <R extends ACTUAL> SELF nonNull(Function<ELEMENT, R> function, MESSAGE message) {
+        return nonNull(function, v -> message);
+    }
 
-    public <R extends ACTUAL> SELF isEmpty(Function<ELEMENT, R> function, Function<ELEMENT, MESSAGE> message);
+    default <R extends ACTUAL> SELF nonNull(Function<ELEMENT, R> function, Supplier<MESSAGE> message) {
+        return nonNull(function, v -> message.get());
+    }
 
-    public SELF isTrue(Function<ELEMENT, Boolean> function, MESSAGE message);
+    <R extends ACTUAL> SELF nonNull(Function<ELEMENT, R> function, Function<ELEMENT, MESSAGE> message);
 
-    public SELF isTrue(Function<ELEMENT, Boolean> function, Function<ELEMENT, MESSAGE> message);
+    default SELF isEmpty(MESSAGE message) {
+        return isEmpty(Function.identity(), message);
+    }
 
-    public SELF isFalse(Function<ELEMENT, Boolean> function, MESSAGE message);
+    default SELF isEmpty(Supplier<MESSAGE> message) {
+        return isEmpty(Function.identity(), e -> message.get());
+    }
 
-    public SELF isFalse(Function<ELEMENT, Boolean> function, Function<ELEMENT, MESSAGE> message);
+    default <R extends ACTUAL> SELF isEmpty(Function<ELEMENT, R> function, MESSAGE message) {
+        return isEmpty(function, v -> message);
+    }
 
-    public <ELEMENT extends ACTUAL> OptionalApiAssert<ELEMENT, ?, MESSAGE, ACTUAL> then(ELEMENT element);
+    default <R extends ACTUAL> SELF isEmpty(Function<ELEMENT, R> function, Supplier<MESSAGE> message) {
+        return isEmpty(function, v -> message.get());
+    }
 
-    public <ELEMENT extends ACTUAL> OptionalApiAssert<ELEMENT, ?, MESSAGE, ACTUAL> then(Supplier<ELEMENT> element);
+    <R extends ACTUAL> SELF isEmpty(Function<ELEMENT, R> function, Function<ELEMENT, MESSAGE> message);
 
-    public <RESULT extends ACTUAL> OptionalApiAssert<RESULT, ?, MESSAGE, ACTUAL> then(Function<ELEMENT, RESULT> element);
+    default SELF isTrue(Function<ELEMENT, Boolean> function, MESSAGE message) {
+        return isTrue(function, v -> message);
+    }
 
-    public <RESULT extends ACTUAL> OptionalApiAssert<RESULT, ?, MESSAGE, ACTUAL> then(BiFunction<ELEMENT, SELF, RESULT> element);
+    default SELF isTrue(Function<ELEMENT, Boolean> function, Supplier<MESSAGE> message) {
+        return isTrue(function, v -> message.get());
+    }
 
-    public SELF process(Consumer<ELEMENT> consumer);
+    SELF isTrue(Function<ELEMENT, Boolean> function, Function<ELEMENT, MESSAGE> message);
 
-    public SELF process(BiConsumer<ELEMENT, SELF> consumer);
+    default SELF isFalse(Function<ELEMENT, Boolean> function, MESSAGE message) {
+        return isFalse(function, v -> message);
+    }
+
+    default SELF isFalse(Function<ELEMENT, Boolean> function, Supplier<MESSAGE> message) {
+        return isFalse(function, v -> message.get());
+    }
+
+    SELF isFalse(Function<ELEMENT, Boolean> function, Function<ELEMENT, MESSAGE> message);
+
+    <ELEMENT extends ACTUAL> OptionalApiAssert<ELEMENT, ?, MESSAGE, ACTUAL> then(ELEMENT element);
+
+    <ELEMENT extends ACTUAL> OptionalApiAssert<ELEMENT, ?, MESSAGE, ACTUAL> then(Supplier<ELEMENT> element);
+
+    <RESULT extends ACTUAL> OptionalApiAssert<RESULT, ?, MESSAGE, ACTUAL> then(Function<ELEMENT, RESULT> element);
+
+    <RESULT extends ACTUAL> OptionalApiAssert<RESULT, ?, MESSAGE, ACTUAL> then(BiFunction<ELEMENT, SELF, RESULT> element);
+
+
+    ELEMENT get();
+
+    default SELF process(Consumer<ELEMENT> consumer) {
+        consumer.accept(get());
+        return self();
+    }
+
+    default SELF process(BiConsumer<ELEMENT, SELF> consumer) {
+        consumer.accept(get(), self());
+        return self();
+    }
 }
--- src/main/java/io/github/min1854/apiAssert/check/abstractAssert/AbstractOperationApiAssert.java
+++ src/main/java/io/github/min1854/apiAssert/check/abstractAssert/AbstractOperationApiAssert.java
@@ -1,12 +1,9 @@
 package io.github.min1854.apiAssert.check.abstractAssert;
 
 import io.github.min1854.apiAssert.api.OptionalApiAssert;
-import io.github.min1854.apiAssert.api.StandardApiAssert;
 import io.github.min1854.apiAssert.exception.ApiAssertException;
 import lombok.Getter;
 
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
@@ -30,18 +27,8 @@ public abstract class AbstractOperationApiAssert<ELEMENT extends ACTUAL, SELF ex
     }
 
     @Override
-    protected void established(Supplier<MESSAGE> message) throws RuntimeException {
-        throw exceptionGenerator.apply(message.get());
-    }
-
-    @Override
-    public SELF nonNull(MESSAGE message) {
-        return super.nonNull(this.obj, message);
-    }
-
-    @Override
-    public <R extends ACTUAL> SELF nonNull(Function<ELEMENT, R> function, MESSAGE message) {
-        return super.nonNull(function.apply(this.obj), message);
+    public <R extends ACTUAL> SELF isNull(Function<ELEMENT, R> function, Function<ELEMENT, MESSAGE> message) {
+        return super.isNull(function.apply(this.obj), message.apply(this.obj));
     }
 
     @Override
@@ -50,42 +37,8 @@ public abstract class AbstractOperationApiAssert<ELEMENT extends ACTUAL, SELF ex
     }
 
     @Override
-    public SELF isNull(MESSAGE message) {
-        return super.isNull(this.obj, message);
-    }
-
-    @Override
-    public <R extends ACTUAL> SELF isNull(Function<ELEMENT, R> function, MESSAGE message) {
-        return super.isNull(function.apply(this.obj), message);
-    }
-
-    @Override
-    public <R extends ACTUAL> SELF isNull(Function<ELEMENT, R> function, Function<ELEMENT, MESSAGE> message) {
-        return super.isNull(function.apply(this.obj), message.apply(this.obj));
-    }
-
-    @Override
-    public <R extends ACTUAL> SELF isEmpty(Function<ELEMENT, R> function, MESSAGE message) {
-        return super.isEmpty(function.apply(this.obj), message);
-    }
-
-    @Override
-    public SELF isEmpty(MESSAGE message) {
-        return super.isEmpty(this.obj, message);
-    }
-
-    @Override
     public <R extends ACTUAL> SELF isEmpty(Function<ELEMENT, R> function, Function<ELEMENT, MESSAGE> message) {
         return super.isEmpty(function.apply(this.obj), message.apply(this.obj));
-    }
-
-    @Override
-    public SELF isTrue(Function<ELEMENT, Boolean> function, MESSAGE message) {
-        Boolean actual = function.apply(this.obj);
-        if (actual == null) {
-            throw new ApiAssertException("返回结果不能为空");
-        }
-        return super.isTrue(actual, message);
     }
 
     @Override
@@ -98,15 +51,6 @@ public abstract class AbstractOperationApiAssert<ELEMENT extends ACTUAL, SELF ex
     }
 
     @Override
-    public SELF isFalse(Function<ELEMENT, Boolean> function, MESSAGE message) {
-        Boolean actual = function.apply(this.obj);
-        if (actual == null) {
-            throw new ApiAssertException("返回结果不能为空");
-        }
-        return super.isFalse(actual, message);
-    }
-
-    @Override
     public SELF isFalse(Function<ELEMENT, Boolean> function, Function<ELEMENT, MESSAGE> messageCreate) {
         Boolean actual = function.apply(this.obj);
         if (actual == null) {
@@ -116,15 +60,13 @@ public abstract class AbstractOperationApiAssert<ELEMENT extends ACTUAL, SELF ex
     }
 
     @Override
-    public SELF process(Consumer<ELEMENT> consumer) {
-        consumer.accept(this.obj);
-        return self();
+    public ELEMENT get() {
+        return this.obj;
     }
 
     @Override
-    public SELF process(BiConsumer<ELEMENT, SELF> consumer) {
-        consumer.accept(this.obj, self());
-        return self();
+    protected void established(Supplier<MESSAGE> message) throws RuntimeException {
+        throw exceptionGenerator.apply(message.get());
     }
 
 }
